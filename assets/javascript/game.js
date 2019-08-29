ce95b4293e038ec54e764e758a443616c9524c3e
Stimoe <stimoe3@gmail.com>
2019-08-29 07:21:44 -0700

    currently have correct and incorrect guesses kind of working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 var wordEL = document.getElementById("word");
-var wrongGuessesEL =[]= document.getElementById("wrongGuesses");
+var wrongGuessesEL = document.getElementById("wrongGuesses");
 var spaces = document.getElementById("computerWord")
 var guessesLeftEL = document.getElementById("guessesLeft");
 var winsEL = document.getElementById("wins");
@@ -9,38 +9,185 @@ var computerChoices = ["thomas", "element"];
 var computerNewWord = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 var spacesToMake= computerNewWord.length;
 var spaces =[]
-var guesses= wrongGuessesEL
+var guesses =[]
+// var guess =[]
+var lives=12
+var currentGuess
+var notInArray=false
+var wrong="p"
+guessesLeftEL.textContent=lives
 
-
-console.log(spacesToMake)
+// Figures out how many spaces to make
 for (var i = 0; i < spacesToMake; i++) {
     spaces.push("_")
-    console.log(spaces)
+    wordEL.textContent=spaces.join(" ")  
+}
+
+document.onkeyup = function(event) {
   
-  wordEL.textContent=spaces.join(" ")
+  console.log("event key", event.key)
+ currentGuess=event.key
+ 
+ for (var i = 0; i < computerNewWord.length; i++) {
+  if (computerNewWord[i]===currentGuess){ 
+    notInArray=true
+    console.log(currentGuess)
+    spaces.splice(i, 1, currentGuess)
+    wordEL.textContent=spaces.join(" ") 
+    console.log("they guessed " + currentGuess)
+    console.log(notInArray)
+  }
 }
-for (var i = 0; i<20; i++) {
-  var userGuesses=[]
-onkeyup = function(guess) {
-  console.log(guess.key)  
+   if(notInArray===false){
+    wrong=currentGuess
+     lives--
+      guesses.push(wrong)
+      console.log(guesses)
+      wrongGuessesEL.textContent=guesses
+      guessesLeftEL.textContent=lives 
+  }
+  
 }
 
-}
-var guesses = [];
 
-document.onkeypress = function(evt) {
-    evt = evt || window.event;
 
-    // Ensure we only handle printable keys
-    var charCode = typeof evt.which == "number" ? evt.which : evt.keyCode;
 
-    if (charCode) {
-        guesses.push(String.fromCharCode(charCode));
-        
-    }
+// if(notInArray=false){
+//   console.log(notInArray)
+//   guesses.push(currentGuess)
+//   console.log(guesses)
+// }
+
+
+//   if (computerNewWord[i]!==currentGuess){
+//     guesses.push(currentGuess)
+//     console.log(guesses)
+// }
+
+
+//   if (notInArray=false) {
+     
+//     lives--
     
-};
+//     // guesses.push(currentGuess)
+    
+//     console.log("these are guesses" + guesses) 
+//     guessesLeftEL.textContent=lives
+//      
+// }
+ 
 
 
-// guess2.appendChild(wrongGuessesEL);
-// appendChild(wrongGuessesEL);
+
+// var string=computerNewWord
+// var  newWordString= string.split("");
+// console.log(newWordString)
+
+// document.onkeyup = function(event) {
+//   console.log("event key", event.key)
+//  currentGuess=event.key
+//  for (var i = 0; i < lives; i++) {
+//   if (newWordString[i]===currentGuess){ 
+//     spaces.splice(i, 1, currentGuess)
+//   wordEL.textContent=spaces
+//     notInArray=true
+//   }
+//  }
+
+//   if (notInArray === false) {
+//     lives--
+    
+//     guesses.push(currentGuess)
+//     console.log(guesses)
+    
+    
+// }
+// wrongGuessesEL.textContent=guesses
+// }
+
+
+ 
+
+
+//   if guess===newWordString
+//   console.log(guess)
+
+  
+  
+  
+//   for (var i = 0; i < newWordString.length; i++) {
+//   if (guess.indexOf(newWordString[i]) > -1) {
+//     found = true;
+    
+//     console.log("same")
+//       break;
+//   }
+// }   
+
+// }   
+    // var charCode = typeof evt.which == "number" ? evt.which : evt.keyCode;
+    // charCode=guess.push(String.fromCharCode(charCode));
+    // console.log(guess)
+
+    // for (var  i= 0; i < newWordString.length; i++) {
+    //   for(var j=0; j< guess.length; j++)
+    //   if (newWordString[i]===guess[j]){
+
+    //     console.log("same")
+    //   }
+      
+
+// for (var i = 0; i < newWordString.length; i++) {
+//     if (guess.indexOf(newWordString[i]) > -1) {
+//         found = true;
+        
+//         console.log("same")
+//         guess.pop()
+//         break;
+//     }
+    
+// }
+
+
+       
+       
+      
+      //  else if (firstLetter==="o") {
+      //     alert(animalAtCurrentIndex + " stars with a o!");
+      //  }
+    // }
+    // for (let  i= 0; i < myFarm.length; i++) {
+    //   var  animalAtCurrentIndex = myFarm[i];
+    //    var firstLetter = animalAtCurrentIndex[0];
+    //    console.log(firstLetter);
+    //    if (firstLetter ==="c") {
+    //        alert(animalAtCurrentIndex + " stars with a c!");
+    //    }
+    //    else if (firstLetter==="o") {
+    //       alert(animalAtCurrentIndex + " stars with a o!");
+    //    }
+    // }
+    // var guesses= guess.slice(0)
+      // guess[0]=(String(charCode))
+      // guess[0]=(String.fromCharCode(charCode))
+    //   console.log(guess)
+    //   console.log(guesses)
+    // for (var i = 0; i < newWordString.length; i++) {
+    //   // charCode=guesses.push(String.fromCharCode(charCode));
+    //   // console.log(charCode)
+    //   if (guess.indexOf(newWordString[i]) > -1) {
+    //   console.log("You guessed " + guess + " right!");
+    //   }
+    //  else 
+    // // (charCode) {
+    //   {
+    //     guesses.push(String.fromCharCode(charCode));
+        
+    //     wrongGuessesEL.textContent=guesses
+    //     console.log(guesses)
+    // }
+    
+  
+
+
+
